7c7439b23e56e90ae870e266591cf1b8602c4b14
Lin <linmaweb@gmail.com>
2020-09-28 16:35:07 +1000

    components updated

--- src/components/elements/Grid/Grid.js
+++ src/components/elements/Grid/Grid.js
@@ -1,33 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Grid.css';
+import React from "react";
+import "./Grid.css";
 
 const Grid = (props) => {
-
   const renderElements = () => {
-    const gridElements = props.children.map( (element, i) => {
+    const gridElements = props.children.map((element, i) => {
       return (
         <div key={i} className="movie-db-grid-element">
           {element}
         </div>
-      )
-    })
+      );
+    });
     return gridElements;
-  }
+  };
 
   return (
     <div className="movie-db-grid">
       {props.header && !props.loading ? <h1>{props.header}</h1> : null}
-      <div className="movie-db-grid-content">
-        {renderElements()}
-      </div>
+      <div className="movie-db-grid-content">{renderElements()}</div>
     </div>
-  )
-}
-
-Grid.propTypes = {
-  header: PropTypes.string,
-  loading: PropTypes.bool.isRequired
-}
+  );
+};
 
 export default Grid;
